0e5d281c34460ea9756e6b0eea2f31a126b34560
Samuel Reiter <samreiter9@gmail.com>
2025-12-03 16:16:45 +0100

    comments = good :)

--- src/train.py
+++ src/train.py
@@ -15,145 +15,24 @@ from matplotlib import pyplot as plt
 import pdb
 from rich.console import Console
 
-class ReplayBuffer:
-    def __init__(
-        self,
-        config: dict,
-        observation_shape: torch.Tensor,
-    ):
-        """
-        A replay buffer for DQN.
-
-        Parameters
-        ----------
-        config : `dict`
-            Configuration dictionary. Required key-value pairs:
-            `"replay_buffer_capacity"` : `int`
-                Capacity of the replay buffer.
-            `"device"` : `torch.device | int | str`
-                Device to store the replay buffer tensors on.
-            `"minibatch_size"` : `int`
-                Minibatch size for sampling from the replay buffer.
-        observation_shape : 'torch.Int64'
-            the shape of the observation shape
-        """
-        # config stuff
-        self.buffer_capacity = config['replay_buffer_capacity']
-        self.device = config['device']
-        self.minibatch_size = config['minibatch_size']
-        self.config = config
-
-        # setup stuff
-
-        self.cursor = 1
-        self.size = 0
-
-        # set up storage tensors
-        self.actions = torch.empty((self.buffer_capacity,), device = self.device, dtype = torch.int64)
-        self.observations = torch.empty((self.buffer_capacity,) + observation_shape, device = self.device, dtype = torch.float32)
-        self.rewards = torch.empty((self.buffer_capacity,), device = self.device, dtype = torch.float32)
-        self.terminal = torch.empty((self.buffer_capacity,), device = self.device, dtype = torch.bool)
-
-
-    def get_minibatch(self) -> dict:
-        """
-        Sample a minibatch from the replay buffer.
-
-        Returns
-        -------
-        A dictionary with the following key-value pairs:
-        - `"actions"` : `torch.Tensor`
-            A tensor of shape `(minibatch_size,)`
-            of actions.
-        - `"observations"` : `torch.Tensor`
-            A tensor of shape `(minibatch_size, observation_dim)`
-            of observations. These are the observations preceding the actions.
-        - `"observations_next"` : `torch.Tensor`
-            A tensor of shape `(minibatch_size, observation_dim)`
-            of next observations. These are the observations following the actions.
-        - `"rewards"` : `torch.Tensor`
-            A tensor of shape `(minibatch_size,)`
-            of rewards.
-        - `"terminal"` : `torch.Tensor`
-            A tensor of shape `(minibatch_size,)`
-            of terminal flags.
-        """
-        indices = torch.randint(0, self.size, (self.minibatch_size,), device = self.device)
-        indices += self.cursor - self.size
-        indices %= self.buffer_capacity
-
-        actions = torch.gather(self.actions, dim = 0, index = indices)
-        observations = torch.gather(
-            self.observations,
-            dim=0,
-            index = indices.unsqueeze(-1)
-            )
-
-        observations_next = torch.gather(
-            self.observations,
-            dim=0,
-            index=indices.unsqueeze(-1).expand(
-                *indices.shape,
-                self.observations.shape[-1]
-            )
-        )
-        rewards = torch.gather(
-            self.rewards,
-            dim=0,
-            index=indices
-        )
-        terminal = torch.gather(
-            self.terminal,
-            dim=0,
-            index=indices
-        )
-
-        minibatch = {
-            "actions": actions,
-            "observations": observations,
-            "observations_next": observations_next,
-            "rewards": rewards,
-            "terminal": terminal,
-        }
-
-        return minibatch
-
-
-    def update(
-        self,
-        action: torch.Tensor,
-        observation: torch.Tensor,
-        reward: torch.Tensor,
-        terminal: torch.Tensor
-    ):
-        """
-        Store new step data at the cursor, then update the cursor and the size. Note that the maximum value of the size is capacity minus 1.
-
-        Parameters
-        ----------
-        actions : `torch.int64`
-            A tensor of shape `` of actions.
-        observations : `torch.Tensor`
-            A tensor of shape ` + (observation_dim,)` of observations.
-        rewards : `torch.float32`
-        terminal : `torch.bool`
-        """
-        assert action.dtype == torch.int64
-        assert reward.dtype == torch.float32
-        assert terminal.dtype == torch.bool
-
-        self.actions[self.cursor] = action
-        self.observations[self.cursor] = observation
-        self.rewards[self.cursor] = reward
-        self.terminal[self.cursor] = terminal
-
-        self.cursor += 1
-        self.cursor %= self.buffer_capacity
-
-        self.size = max(self.buffer_capacity - 1, self.size + 1)
-
 def play_game(players, game):
-        
+    '''
+    play a single game given the players
+
+    ---
+    arguments:
+    - players (list of models): the players
+    - game (env): the enviornment to play in
+
+    ---
+    returns:
+    - memory (list of dict): a set of memories for each player
+        - rewards (list): the rewards after the next players move at each move
+        - observations (list): the observations for each player
+        - probabilities (list): the probability that the chosen move should have been chosen
+    '''
+
+    # set up    
     memory = [
         {
             "rewards": [],
@@ -168,14 +47,17 @@ def play_game(players, game):
 
     while not terminal:
         player = turn % 2
-        action_probabilities = players[player](state)
 
+        # get active players probability distribution and action
+        action_probabilities = players[player](state)
         dist = Categorical(logits=action_probabilities)
-        action = dist.sample()                         # shape (1,)
-        log_prob = dist.log_prob(action).squeeze(0)    # scalar
-        # pdb.set_trace()
+        action = dist.sample()                         
+        log_prob = dist.log_prob(action).squeeze(0)
+
+        # play the move
         state, reward, terminal = game.step(action.numpy())
 
+        # convert to torch
         state = torch.asarray(state, device = config['device'], dtype = torch.float32)
         reward = torch.asarray(reward, device = config['device'], dtype = torch.float32)
         terminal = torch.asarray(terminal, device = config['device'], dtype = torch.bool)
@@ -187,44 +69,62 @@ def play_game(players, game):
             memory[(player + 1) % 2]['observations'].append(last_state)
             memory[(player + 1) % 2]['rewards'].append(reward[(player + 1) % 2])
 
+        # set up for next turn
         last_log_prob = log_prob
         last_state = state
         turn += 1
 
-    assert len(last_log_prob.shape) == 0 
+    # save the last turn for the active player
     memory[player]['probabilities'].append(last_log_prob)
     memory[player]['observations'].append(last_state)
     memory[player]['rewards'].append(reward[player])
     
+    # stack all lists
     for player in range(2):
         for key in memory[player].keys():
-            # pdb.set_trace()
             memory[player][key] = torch.stack(memory[player][key])
     return memory
 
 
 def train(config, players):
+    """
+    train the two models
+
+    returns:
+    - log (dict):
+        - player1_rewards (list): the rewards for the first player
+        - player1_score (list): the sum of the rewards up to that point
+    """
+
+    # set up
     optimizers = [AdamW(players[i].parameters()) for i in range(len(players))]
     log = {
         'player1_rewards': [],
         'player1_score': []
     }
-
     progress_bar = tqdm(range(config['train_steps']))
+
     for game_id in progress_bar:
+        # play a game
         memory = play_game(players, kuhn)
+
+        # save results to log
         log['player1_rewards'].append(memory[0]['rewards'][-1])
         if game_id >= 1:
             log['player1_score'].append(log['player1_score'][-1] + log['player1_rewards'][-1])
         else:
             log['player1_score'].append(log['player1_rewards'][-1])
             
-
+        # back propogation
         for player in range(len(players)):
+            # get undiscounted rewards (undiscounted since number of moves shouldn't matter)
             if len(memory[player]['rewards']) > 1:
                 rewards = torch.tensor([sum(memory[player]['rewards'][move:]) for move in range(len(memory[player]['rewards']))], device= config['device'], dtype = torch.float32)
             else:
                 rewards = memory[player]['rewards'][0]
+
+            # loss and backprop
+            # CHECK MAKE SURE THIS LOSS FUNCTION IS OPTIMAL
             optimizers[player].zero_grad()
             loss = -1 * ((memory[player]['probabilities'] * rewards).sum())
             loss.backward()
@@ -244,6 +144,7 @@ if __name__ == '__main__':
 
         kuhn = KuhnPoker()
 
+        # make players
         player1 = Sequential(
             Linear(12, 10),
             Linear(10, 10),
@@ -262,8 +163,10 @@ if __name__ == '__main__':
             player2
         ]
 
+        # train
         log = train(config, players)
         
+        # visualize
         plt.plot(log['player1_score'])
         plt.show()
 
